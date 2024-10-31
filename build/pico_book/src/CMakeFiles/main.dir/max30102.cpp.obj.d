1c93bfa21827a06c266b7e03d6328375218da006
zhc <your.email@example.com>
2024-10-31 16:47:44 +0000

    MAX30102 iIC链接成功

--- build/pico_book/src/CMakeFiles/main.dir/max30102.cpp.obj.d
+++ build/pico_book/src/CMakeFiles/main.dir/max30102.cpp.obj.d
@@ -1,5 +1,6 @@
-pico_book/src/CMakeFiles/main.dir/main.cpp.obj: \
- /home/pico-book/pico_book/src/main.cpp /usr/include/newlib/stdio.h \
+pico_book/src/CMakeFiles/main.dir/max30102.cpp.obj: \
+ /home/pico-book/pico_book/src/max30102.cpp \
+ /home/pico-book/pico_book/src/max30102.h /usr/include/newlib/stdio.h \
  /usr/include/newlib/_ansi.h /usr/include/newlib/newlib.h \
  /usr/include/newlib/_newlib_version.h /usr/include/newlib/sys/config.h \
  /usr/include/newlib/machine/ieeefp.h /usr/include/newlib/sys/features.h \
@@ -68,25 +69,8 @@ pico_book/src/CMakeFiles/main.dir/main.cpp.obj: \
  /usr/local/picosdk/src/rp2040/hardware_structs/include/hardware/structs/resets.h \
  /usr/local/picosdk/src/rp2040/hardware_regs/include/hardware/regs/resets.h \
  /usr/local/picosdk/src/rp2_common/pico_stdio_uart/include/pico/stdio_uart.h \
- /usr/local/picosdk/src/rp2_common/pico_multicore/include/pico/multicore.h \
- /usr/local/picosdk/src/common/pico_sync/include/pico/sync.h \
- /usr/local/picosdk/src/common/pico_sync/include/pico/sem.h \
- /usr/local/picosdk/src/common/pico_sync/include/pico/lock_core.h \
- /usr/local/picosdk/src/rp2_common/hardware_sync/include/hardware/sync.h \
- /usr/local/picosdk/src/rp2_common/hardware_sync_spin_lock/include/hardware/sync/spin_lock.h \
- /usr/local/picosdk/src/common/pico_sync/include/pico/mutex.h \
- /usr/local/picosdk/src/common/pico_sync/include/pico/critical_section.h \
- /usr/local/freertos/include/FreeRTOS.h \
- /usr/local/freertos/include/projdefs.h \
- /usr/local/freertos/include/portable.h \
- /usr/local/freertos/include/deprecated_definitions.h \
- /usr/local/freertos/portable/ThirdParty/GCC/RP2040/include/portmacro.h \
- /usr/local/freertos/include/mpu_wrappers.h \
- /usr/local/freertos/include/task.h /usr/local/freertos/include/list.h \
- /usr/local/freertos/include/event_groups.h \
- /usr/local/freertos/include/timers.h /usr/local/freertos/include/task.h \
- /usr/local/freertos/include/queue.h /home/pico-book/pico_book/src/Mp3.h \
- /home/pico-book/pico_book/src/max30102.h \
  /usr/local/picosdk/src/rp2_common/hardware_i2c/include/hardware/i2c.h \
  /usr/local/picosdk/src/rp2040/hardware_structs/include/hardware/structs/i2c.h \
- /usr/local/picosdk/src/rp2040/hardware_regs/include/hardware/regs/i2c.h
+ /usr/local/picosdk/src/rp2040/hardware_regs/include/hardware/regs/i2c.h \
+ /usr/include/newlib/string.h /usr/include/newlib/sys/_locale.h \
+ /usr/include/newlib/strings.h /usr/include/newlib/sys/string.h
---
+++ build/pico_book/src/CMakeFiles/main.dir/usr/local/picosdk/src/rp2_common/hardware_i2c/i2c.c.obj.d
@@ -0,0 +1,49 @@
+pico_book/src/CMakeFiles/main.dir/usr/local/picosdk/src/rp2_common/hardware_i2c/i2c.c.obj: \
+ /usr/local/picosdk/src/rp2_common/hardware_i2c/i2c.c \
+ /usr/local/picosdk/src/rp2_common/hardware_i2c/include/hardware/i2c.h \
+ /usr/local/picosdk/src/common/pico_base_headers/include/pico.h \
+ /usr/local/picosdk/src/common/pico_base_headers/include/pico/types.h \
+ /usr/local/picosdk/src/common/pico_base_headers/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
+ /usr/include/newlib/assert.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ /home/pico-book/build/generated/pico_base/pico/version.h \
+ /usr/local/picosdk/src/common/pico_base_headers/include/pico/config.h \
+ /home/pico-book/build/generated/pico_base/pico/config_autogen.h \
+ /usr/local/picosdk/src/boards/include/boards/pico.h \
+ /usr/local/picosdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /usr/local/freertos/portable/ThirdParty/GCC/RP2040/include/freertos_sdk_config.h \
+ /home/pico-book/pico_book/src/../FreeRTOSConfig.h \
+ /home/pico-book/pico_book/FreeRTOSConfig_examples_common.h \
+ /usr/local/freertos/portable/ThirdParty/GCC/RP2040/include/rp2040_config.h \
+ /usr/local/picosdk/src/rp2040/pico_platform/include/pico/platform.h \
+ /usr/local/picosdk/src/rp2_common/pico_platform_compiler/include/pico/platform/compiler.h \
+ /usr/local/picosdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/local/picosdk/src/common/pico_base_headers/include/pico/types.h \
+ /usr/local/picosdk/src/rp2_common/pico_platform_sections/include/pico/platform/sections.h \
+ /usr/local/picosdk/src/rp2_common/pico_platform_panic/include/pico/platform/panic.h \
+ /usr/local/picosdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /usr/local/picosdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /usr/local/picosdk/src/common/pico_base_headers/include/pico/error.h \
+ /usr/local/picosdk/src/common/pico_time/include/pico/time.h \
+ /usr/local/picosdk/src/rp2_common/hardware_timer/include/hardware/timer.h \
+ /usr/local/picosdk/src/rp2040/hardware_structs/include/hardware/structs/timer.h \
+ /usr/local/picosdk/src/rp2_common/hardware_base/include/hardware/address_mapped.h \
+ /usr/local/picosdk/src/rp2040/hardware_regs/include/hardware/regs/timer.h \
+ /usr/local/picosdk/src/rp2040/hardware_regs/include/hardware/regs/intctrl.h \
+ /usr/local/picosdk/src/rp2040/hardware_structs/include/hardware/structs/i2c.h \
+ /usr/local/picosdk/src/rp2040/hardware_regs/include/hardware/regs/i2c.h \
+ /usr/local/picosdk/src/rp2040/hardware_regs/include/hardware/regs/dreq.h \
+ /usr/local/picosdk/src/rp2_common/hardware_resets/include/hardware/resets.h \
+ /usr/local/picosdk/src/rp2040/hardware_structs/include/hardware/structs/resets.h \
+ /usr/local/picosdk/src/rp2040/hardware_regs/include/hardware/regs/resets.h \
+ /usr/local/picosdk/src/rp2_common/hardware_clocks/include/hardware/clocks.h \
+ /usr/local/picosdk/src/rp2040/hardware_structs/include/hardware/structs/clocks.h \
+ /usr/local/picosdk/src/rp2040/hardware_regs/include/hardware/regs/clocks.h \
+ /usr/local/picosdk/src/common/pico_time/include/pico/timeout_helper.h
